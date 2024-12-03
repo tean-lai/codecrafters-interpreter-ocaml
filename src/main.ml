b4d8adbb0e743ea0414fabcaa73a3854b43004cd
tean <laitean19@gmail.com>
2024-12-03 17:49:11 -0500

    bug fix

--- src/main.ml
+++ src/main.ml
@@ -10,59 +10,71 @@ type token =
   | SEMICOLON
   | STAR
 
+let had_error = ref false
+
+let report line where message =
+  Printf.eprintf "[line %d] Error%s: %s\n" line where message;
+  had_error := true
+
+let error line message = report line "" message
+
 let scan s =
-  let rec helper seq acc =
+  let rec helper seq acc line =
     match seq () with
     | Seq.Nil -> List.rev acc
     | Seq.Cons (h, t) -> (
         match h with
-        | '(' -> helper t (LEFT_PAREN :: acc)
-        | ')' -> helper t (RIGHT_PAREN :: acc)
-        | '{' -> helper t (LEFT_BRACE :: acc)
-        | '}' -> helper t (RIGHT_BRACE :: acc)
-        | '.' -> helper t (DOT :: acc)
-        | ',' -> helper t (COMMA :: acc)
-        | '*' -> helper t (STAR :: acc)
-        | '+' -> helper t (PLUS :: acc)
-        | '-' -> helper t (MINUS :: acc)
-        | ';' -> helper t (SEMICOLON :: acc)
+        | '(' -> helper t (LEFT_PAREN :: acc) line
+        | ')' -> helper t (RIGHT_PAREN :: acc) line
+        | '{' -> helper t (LEFT_BRACE :: acc) line
+        | '}' -> helper t (RIGHT_BRACE :: acc) line
+        | '.' -> helper t (DOT :: acc) line
+        | ',' -> helper t (COMMA :: acc) line
+        | '*' -> helper t (STAR :: acc) line
+        | '+' -> helper t (PLUS :: acc) line
+        | '-' -> helper t (MINUS :: acc) line
+        | ';' -> helper t (SEMICOLON :: acc) line
         | _ ->
-            Printf.printf "[line 1] Error: Unexpected character: %c\n" h;
-            helper t acc)
+            error line ("Unexpected character: " ^ String.make 1 h);
+            helper t acc line)
   in
-  helper (String.to_seq s) []
+  helper (String.to_seq s) [] 1
+
+let eprint_endline s =
+  Printf.eprintf s;
+  Printf.eprintf "\n"
 
 let rec pprint_tokens = function
-  | [] -> print_endline "EOF  null"
+  | [] -> eprint_endline "EOF  null"
   | LEFT_PAREN :: t ->
-      print_endline "LEFT_PAREN ( null";
+      eprint_endline "LEFT_PAREN ( null";
       pprint_tokens t
   | RIGHT_PAREN :: t ->
-      print_endline "RIGHT_PAREN ) null";
+      eprint_endline "RIGHT_PAREN ) null";
       pprint_tokens t
   | LEFT_BRACE :: t ->
-      print_endline "LEFT_BRACE { null";
+      eprint_endline "LEFT_BRACE { null";
       pprint_tokens t
   | RIGHT_BRACE :: t ->
-      print_endline "RIGHT_BRACE } null";
+      eprint_endline "RIGHT_BRACE } null";
       pprint_tokens t
   | COMMA :: t ->
-      print_endline "COMMA , null";
+      eprint_endline "COMMA , null";
       pprint_tokens t
   | DOT :: t ->
-      print_endline "DOT . null";
+      eprint_endline "DOT . null";
       pprint_tokens t
   | STAR :: t ->
-      print_endline "STAR * null";
+      eprint_endline "STAR * null";
       pprint_tokens t
   | PLUS :: t ->
-      print_endline "PLUS + null";
+      eprint_endline "PLUS + null";
       pprint_tokens t
   | MINUS :: t ->
-      print_endline "MINUS - null";
+      eprint_endline "MINUS - null";
       pprint_tokens t
   | SEMICOLON :: t ->
-      print_endline "SEMICOLON ; null";
+      eprint_endline "SEMICOLON ; null";
       pprint_tokens t
 
 (* | _ -> failwith "todo" *)
